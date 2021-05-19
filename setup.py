1cef0c703b22afc88e12804d7b8dce386855f7a4
Luc Geven <lucgeven@MacBook-Pro-van-Luc.local>
2021-05-19 16:02:01 +0200

    Initial outline project

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='AOTK',
+    version = '0.1',
+    description='AOTK tool',
+    author='Luc Geven, Benjamin van Seggelen, Roy van Schaijk',
+    packages=find_packages()
+)
